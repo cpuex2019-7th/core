caee12de8c0d73c47566635681984d591bd6476c
hashi0203 <n.hsmt1123@gmail.com>
2019-11-27 21:11:19 +0900

    fadd and fsub divided

--- src/fpu/fsub.sv
+++ src/fpu/fsub.sv
@@ -3,10 +3,14 @@
 module fsub
    (  input wire [31:0]  x1,
       input wire [31:0]  x2,
+		input wire         clk,
+		input wire         rstn,
+		input wire         enable_in,
+		output wire        enable_out,
       output wire [31:0] y,
       output wire        ovf);
 
-   fadd u1(x1,{~x2[31:31],x2[30:0]},y,ovf);
+   fadd u1(x1,{~x2[31:31],x2[30:0]},clk,rstn,enable_in,enable_out,y,ovf);
 
 endmodule                                                                         
 `default_nettype wire
