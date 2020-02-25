84397b7627dbdacd2414eb9f60c56f16151daf7d
Takashi Yoneuchi <takashi.yoneuchi@shift-js.info>
2020-02-26 00:56:30 +0900

    reduce fetch phase

--- src/fetch.sv
+++ src/fetch.sv
@@ -12,14 +12,15 @@ module fetch
   
    output wire        completed,
    output reg [31:0] pc_n,
-   output reg [31:0] instr_raw);
+   output wire [31:0] instr_raw);
 
    reg               state;
    reg _completed;
    assign completed = _completed & !enabled;
    
    assign rom_addr = pc;     
-
+   assign insr_raw = rom_data;
+   
    // initialize
    initial begin
       state <= 0;      
@@ -30,15 +31,8 @@ module fetch
       if(rstn) begin
          if (enabled) begin
             state <= 0;
-            _completed <= 0;
-            pc_n <= pc;
-         end else if (state == 0) begin
-            state <= 1;            
-         end else if (state == 1) begin
-            state <= 0;
-            
             _completed <= 1;
-            instr_raw <= rom_data;            
+            pc_n <= pc;
          end
       end else begin
          state <= 0;
